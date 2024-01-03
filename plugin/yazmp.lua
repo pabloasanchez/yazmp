f7dd73eb5f5d47e08b01a8bb49110096af6fad3b
Pablo <pabloasanchez+github@gmail.com>
2024-01-03 10:36:59 +0000

    Update yazmp.lua

--- plugin/yazmp.lua
+++ plugin/yazmp.lua
@@ -1,3 +1,7 @@
 vim.api.nvim_create_user_command("Zenmode", function(c)
-	require("yazmp").zenmode(c)
+  require("yazmp").zenmode(c)
 end, { nargs = "?" })
+
+vim.api.nvim_create_user_command("ZenmodeClose", function(c)
+  require("yazmp").closezenmode(c)
+end, {})
